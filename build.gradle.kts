9f37a17519329e93434bf7b7a4b1519b50959378
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-08-14 15:59:09 +0200

    Bump Compose to Build310 (#10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.5.21" apply false
     kotlin("plugin.serialization") version "1.5.21" apply false
-    id("org.jetbrains.compose") version "1.0.0-alpha3" apply false
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build310" apply false
 }
 
 repositories {

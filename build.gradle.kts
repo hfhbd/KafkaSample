78045dbe06caa6afc773eb7e43f462233de45314
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-09-27 19:34:58 +0200

    Bump Kotlin and Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "1.5.30" apply false
-    kotlin("plugin.serialization") version "1.5.30" apply false
-    id("org.jetbrains.compose") version "1.0.0-alpha4-build328" apply false
+    kotlin("multiplatform") version "1.5.31" apply false
+    kotlin("plugin.serialization") version "1.5.31" apply false
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build362" apply false
 }
 
 repositories {
